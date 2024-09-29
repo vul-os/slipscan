5e1b67ef4143b49098b7454960296078b62ac7e4
imranparuk <imranparuk@live.com>
2024-09-29 15:20:12 +0200

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@/components/theme-provider';
 import { AuthProvider } from './context/auth-context';
 import AppRoutes from './routes';
 import { Toaster } from "@/components/ui/toaster"
-import './services/firebase';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "slipscan-b6484.firebaseapp.com",
+  projectId: "slipscan-b6484",
+  storageBucket: "slipscan-b6484.appspot.com",
+  messagingSenderId: "***REMOVED***",
+  appId: "1:***REMOVED***:web:e56ec21527ec4a89a89f49",
+  measurementId: "G-N3VXB1GWPY"
+};
 
 const App = () => {
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    // You can add any additional analytics setup here
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AuthProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,14 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@/components/theme-provider';
-import { AuthProvider } from './context/auth-context';
-import AppRoutes from './routes';
-import { Toaster } from "@/components/ui/toaster"
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import App from './app';
+import './index.css';
 
-const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: "slipscan-b6484.firebaseapp.com",
-  projectId: "slipscan-b6484",
-  storageBucket: "slipscan-b6484.appspot.com",
-  messagingSenderId: "***REMOVED***",
-  appId: "1:***REMOVED***:web:e56ec21527ec4a89a89f49",
-  measurementId: "G-N3VXB1GWPY"
-};
-
-const App = () => {
-  useEffect(() => {
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    // You can add any additional analytics setup here
-  }, []);
-
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider>
-        <Router>
-          <AppRoutes />
-        </Router>
-        <Toaster />
-      </AuthProvider>
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <App />
     </ThemeProvider>
-  );
-};
-
-export default App;
+  </React.StrictMode>
+);
